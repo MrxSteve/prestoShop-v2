e0b62b4fa961c66618ad8d1a539c88ecda44d1cd
mrxSteve <steventobar38@gmail.com>
2025-12-05 22:31:30 -0600

    feat: configuracion inicial del proyecto y dependencias

---
+++ src/main/java/com/devsteve/prestashopv2_backend/Prestashopv2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.devsteve.prestashopv2_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prestashopv2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prestashopv2BackendApplication.class, args);
+	}
+
+}
